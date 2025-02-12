0cff38ddca13c95a1eee24baa72115cafab2ef42
GeoBradDev <Brad.Stricherz@gmail.com>
2025-02-11 18:52:34 -0600

    Email server working and media directory added

--- WebGIS/settings.py
+++ WebGIS/settings.py
@@ -140,21 +140,34 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+# Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# Add these settings anywhere in the file.
+# CORS settings
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = ["http://localhost:5173"]
 CSRF_TRUSTED_ORIGINS = ['http://localhost:5173']
 
+# Custom user model
 AUTH_USER_MODEL = 'api.CustomUser'
+
+# Email settings
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER =os.getenv('EMAIL_HOST_USER').strip()
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD').strip()
+DEFAULT_FROM_EMAIL = os.getenv('EMAIL_HOST_USER').strip()
