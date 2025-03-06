e31ffc4e2f8f8740469ffe7dc3227528d04e5a3d
GeoBradDev <Brad.Stricherz@gmail.com>
2025-03-05 20:04:54 -0600

    modified Staticfilesstorage

--- WebGIS/settings.py
+++ WebGIS/settings.py
@@ -147,9 +147,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-if not DEBUG:
-    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Media files
 MEDIA_URL = '/media/'
