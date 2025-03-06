cb282773743ff712ddf7c08a7c0fb3c7e9b0f546
GeoBradDev <Brad.Stricherz@gmail.com>
2025-03-05 20:38:09 -0600

    added Brotli to requirements.txt

--- WebGIS/settings.py
+++ WebGIS/settings.py
@@ -146,8 +146,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+    STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
 
 # Media files
 MEDIA_URL = '/media/'
