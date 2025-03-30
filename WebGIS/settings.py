7d90ab3a8b726c880c91fa2ff791b3c75fdccbe6
GeoBradDev <Brad.Stricherz@gmail.com>
2025-03-29 20:03:58 -0500

    redeploying

--- WebGIS/settings.py
+++ WebGIS/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
-from django.conf.global_settings import SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
