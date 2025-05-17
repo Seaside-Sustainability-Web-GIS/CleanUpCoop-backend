4540575706efb17d487fe98e902bb58f84b90907
Brad Stricherz <Brad.Stricerz@gmail.com>
2025-05-17 12:06:03 -0500

    Updates before adding teams

--- WebGIS/settings.py
+++ WebGIS/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = ['localhost']  # Add your production domain here
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']  # Add your production domain here
 
 # Session settings
 SESSION_ENGINE = "django.contrib.sessions.backends.db"
