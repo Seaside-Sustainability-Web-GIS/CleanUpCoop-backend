df2e5659dc99722fa3a4a465a935045cdde84eb9
Brad Stricherz <Brad.Stricerz@gmail.com>
2025-08-30 16:50:36 -0500

    Update settings.py to set ALLOWED_HOSTS and CORS_ALLOWED_ORIGINS for production

--- WebGIS/settings.py
+++ WebGIS/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = ['ALLOWED_HOSTS']
+ALLOWED_HOSTS = ['cleanupcoop-backend.onrender.com']
 
 # Session settings
 SESSION_ENGINE = "django.contrib.sessions.backends.db"
@@ -38,7 +38,9 @@ SESSION_COOKIE_SECURE = True
 # CORS settings
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOWED_ORIGINS = ['CORS_ALLOWED_ORIGINS']
+CORS_ALLOWED_ORIGINS = [
+    "https://cleanupcoop-frontend.onrender.com",
+]
 CORS_ALLOW_HEADERS = [
     "Content-Type",
     "Authorization",
