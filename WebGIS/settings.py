e19bbd6c91cf982de99414ee7f479f8b853a1657
Brad Stricherz <Brad.Stricerz@gmail.com>
2025-08-30 16:25:46 -0500

    Update settings.py to use placeholder values for ALLOWED_HOSTS and CORS_ALLOWED_ORIGINS

--- WebGIS/settings.py
+++ WebGIS/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = ['seaside-backend-oh06.onrender.com']  # Add your production domain here
+ALLOWED_HOSTS = ['ALLOWED_HOSTS']  # Add your production domain here
 
 # Session settings
 SESSION_ENGINE = "django.contrib.sessions.backends.db"
@@ -39,7 +39,7 @@ SESSION_COOKIE_SECURE = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
-    "https://seaside-frontend.onrender.com",
+    "CORS_ALLOWED_ORIGINS",
 ]
 CORS_ALLOW_HEADERS = [
     "Content-Type",
